59c70ca615325bdcfcbee5c4ec8c435fa3ddf33b
maick400 <maick400@hotmail.com>
2023-06-19 08:44:31 -0500

    Creación de plantillas para e frontend

--- moduloSocios/views.py
+++ moduloSocios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import permission_required, login_required
 from moduloSocios.forms.forms  import *
+from django.db.models import Q
 
 
 
@@ -11,10 +12,14 @@ from moduloSocios.forms.forms  import *
 def crear_socio(request):
     
     pag_titulo = 'Crear Usuario'
+    
     frm_crear = frm_crear_socio
+    filtros = Frm_filtros_busqueda
+    socios = Socio.objects.all()
+
     
     if request.method == 'GET':
-        return render(request, 'socios/socio_crear.html', {'title':pag_titulo,'frm':frm_crear})
+        return render(request, 'socios/socio_crear.html', {'title':pag_titulo,'frm':frm_crear, 'filtros':filtros ,'socios':socios})
     
     
     if request.method  == 'POST':
@@ -30,3 +35,16 @@ def crear_socio(request):
 
 def modify(request):
     pass
+
+def buscar_socio(request):
+    if request.method == 'POST':
+        cedula = request.POST.get("cedula")
+    
+        socios = Socio.objects.all().filter( Q(cedula__icontains= cedula) | Q(nombres__icontains=cedula) )
+        return render (request, 'socios/tables/seleccionar-socio-tabla.html', 
+                       { 
+                        'socios':socios
+                        } )
+    else:
+        pass
+    
