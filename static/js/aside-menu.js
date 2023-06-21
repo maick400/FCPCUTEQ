38103011ec3829c322639191082d947b150723ad
jhonleturne192005 <jleturnep@uteq.edu.ec>
2023-06-21 11:28:42 -0500

    pantalla mejorada

--- static/js/aside-menu.js
+++ static/js/aside-menu.js
@@ -1,15 +1,29 @@
+
 let arrow = document.querySelectorAll(".arrow");
 for (var i = 0; i < arrow.length; i++) {
   arrow[i].addEventListener("click", (e)=>{
- let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
- arrowParent.classList.toggle("showMenu");
+    let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+    arrowParent.classList.toggle("showMenu");
   });
 }
 
+let setText=true
 let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".btn-menu");
 console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", ()=>{
+  let equipo=document.getElementById("equipo");
   sidebar.classList.toggle("close");
+  if(setText){
+    equipo.innerHTML="<b>DevCoders</b>"
+    setText=false
+  }else{
+    equipo.innerHTML=""
+    setText=true
+  }
 });
+
+
+
+
 
