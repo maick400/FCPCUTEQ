897d242a976b5fe45df96920f57fd23e09ded96d
maick400 <miguel.pina2017@uteq.edu.ec>
2023-06-28 09:48:43 -0500

    INtegracion casanova

--- core/tools/get_field.py
+++ core/tools/get_field.py
@@ -5,35 +5,40 @@ from django.db import connection
 class Core_Tabla():
     def __init__(self, name ) -> None:
         self.nombre_tabla = name
-        # self.consulta = self.set_consulta()
-        # self.id_tabla = self.consulta[0][1]
-        # self.numero_tabla = self.consulta[0][3]
-        # self.cantidad_columnas = self.consulta[0][0]
-        # self.cantidad_registros = len(self.consulta)
-        # self.columnas = self.get_columnas()
+        self.consulta = self.set_consulta()
+        self.id_tabla = self.consulta[0][1]
+        self.numero_tabla = self.consulta[0][3]
+        self.cantidad_columnas = self.consulta[0][0]
+        self.cantidad_registros = len(self.consulta)
+        self.columnas = self.get_columnas()
+        self.datos, self.filas_tabla = self.get_datos()  
+        self.choices = self.get_choices()
+        self.table_dict  = self.filas_diccionario()
         
-        # self.datos, self.filas_tabla = self.get_datos()
-        
-        # self.choices = self.get_choices()
-         
-#     def set_consulta(self):
-#         with connection.cursor() as cursor:
-#             cursor.execute("select  * from core_get_tablas('" + self.nombre_tabla + "')")
-#             rows = cursor.fetchall()
-#             return list(rows )
-    
-#     def get_datos(self):
-#         datos = []
-#         tabla = []
-#         campos = []
-#         flag = 1 
-#         for dato in range(self.cantidad_registros):
-#             datos.append({
-#                 'id_valor':self.consulta[dato][7], 
-#                 'valor':self.consulta[dato][8], 
-#                 'activo':self.consulta[dato][9], 
-#                 'linea':self.consulta[dato][10], 
-#                 'id_campo_id':self.consulta[dato][11]})
+    def set_consulta(self):
+        with connection.cursor() as cursor:
+            cursor.execute("select  * from obtener_tabla_con_parametro('" + str(self.nombre_tabla) + "')")
+            rows = cursor.fetchall()
+            
+            if len(rows) == 0: 
+                return None
+            
+            return list(rows )
+    def get_datos(self):
+        if len(self.consulta) == None:
+            return None
+
+        datos = []
+        tabla = []
+        campos = []
+        flag = 1 
+        for dato in range(self.cantidad_registros):
+            datos.append({
+                'id_valor':self.consulta[dato][7], 
+                'valor':self.consulta[dato][8], 
+                'activo':self.consulta[dato][9], 
+                'linea':self.consulta[dato][10], 
+                'id_campo_id':self.consulta[dato][11]})
   
 #             if flag <= self.cantidad_columnas: 
 #                 campos.append(str(self.consulta[dato][8]))
@@ -50,35 +55,77 @@ class Core_Tabla():
             
 #         return datos, tabla
             
-            
-#     def get_numero_registros(self):
-#         return self.consulta.count()
+    def filas_diccionario(self): 
+        if len(self.consulta) == None:
+            return None
+        
+        datos = []
+        aux = {}
+        for fila in self.filas_tabla:
+            for index, dato in enumerate(fila):
+                if index % 2 == 0: 
+                    aux['valor'] =  dato
+                else: 
+                    aux['id'] =  dato
+            datos.append(aux)
+        return datos
+
+                    
+                
+                
+    def get_numero_registros(self):
+        if len(self.consulta) == None:
+            return None
+        
+        return self.consulta.count()
     
-#     def get_choices(self):
-#         tuple = ()
-#         for fila  in self.filas_tabla:
-#             identificador =  fila[0]
+    def get_choices(self):
+        if len(self.consulta) == None:
+            return None
+        
+        tuple = ()
+        display =  ''
+        
+        
+        for fila  in self.filas_tabla:
             
-#             aux = (
-#                 identificador, #Valie del select
-#                   fila[2],  #display del input
-#                 )
+            for i in range(0,self.cantidad_columnas):
+                
+                if i != self.cantidad_columnas - 1:
+                    display = display+ fila[i*2 ] + '  -  '
+                else: 
+                    display = display+ fila[i*2]
+            
+            aux = (
+                fila[0], #Valie del select
+                  display,  #display del input
+                )
+            
+            display =  ''
+
     
 #             tuple = (*tuple, aux)
 #         return   tuple
     
-#     def get_columnas(self):
-#         columnas = []
-#         for columna in range(self.cantidad_columnas):
-#             columnas.append({
-#                 'id':self.consulta[columna][4], 
-#                 'nombre_campo':self.consulta[columna][5],
-#                 })
+    def get_columnas(self):
+        if len(self.consulta) == None:
+            return None
+        
+   
+        
+        columnas = []
+        for columna in range(self.cantidad_columnas):
+            columnas.append({
+                'id':self.consulta[columna][4], 
+                'nombre_campo':self.consulta[columna][5],
+                })
             
 #         return columnas
     
     
 
+
+
             
 
         
@@ -91,14 +138,13 @@ class Core_Tabla():
         
         
         
-        
 
-# # def get_choices(cmd):
-# #     with connection.cursor() as cursor:
-# #             cursor.execute("select  * from obtener_tabla('Código de provincia ')")
-# #             rows = cursor.fetchall()
-# #             resgistros = rows.count()
-# #             numero_columnas = rows[0][2]
+# def get_choices(cmd):
+#     with connection.cursor() as cursor:
+#             cursor.execute("select  * from obtener_tabla('Código de provincia ')")
+#             rows = cursor.fetchall()
+#             resgistros = rows.count()
+#             numero_columnas = rows[0][2]
             
 # #             iterables = int(resgistros/numero_columnas)
 # #             tuple = ()
