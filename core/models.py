b0f60c1a5baea11a6b48519aa1514065695ec02a
maick400 <miguel.pina2017@uteq.edu.ec>
2023-06-30 01:27:10 -0500

    Desvinculacion de meta base de datos

--- core/models.py
+++ core/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 from core.model.Model_CORE_solicitudes_generales import *
 
-from core.model.Model_CORE_tabla import *
-from core.model.Model_CORE_campos import *
-from core.model.Model_CORE_valores import *
+# from core.model.Model_CORE_tabla import *
+# from core.model.Model_CORE_campos import *
+# from core.model.Model_CORE_valores import *
