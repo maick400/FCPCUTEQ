7a9a653c55da8a9a1078fb373319864508057fec
Hecmi <hcasanovam@uteq.edu.ec>
2023-07-01 15:41:38 -0500

    Actualizar

--- moduloFondo/urls.py
+++ moduloFondo/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from moduloFondo.forms import *
 from moduloFondo.views import *
 
+from moduloFondo.vw_casanova import *
+
 app_name = 'fondo'
 urlpatterns = [
     
-    path('operadora/registrar/', crear_operadora_telefonica, name='crear_operadora_telefonica'),
+    path('operadora/registrar/', crear_operadora_telefonica, name='crear_operadora_telefonica'), #type:ignore    
     path('operadora/listar/', listar_operadora_telefonica, name='listar_operadora_telefonica'),
     path('operadora/editar/<pk>', editar_operadora_telefonica, name='editar_operadora_telefonica'),
     path('operadora/buscar/', buscar_operadora, name='buscar_operadora'),
@@ -18,22 +20,25 @@ urlpatterns = [
     #----------------------RUTAS SOCIO----------------------#
 
     #----------------------RUTAS PROVINCIAS----------------------#
-    path('provincias/lista/', listar_provincias, name='listar_provincias'),
-    path('provincias/registrar/', agregar_provincia, name='agregar_provincia'),
-    path('provincias/editar/<pk>', editar_provincia, name='editar_provincia'),
+    path('provincias/listar/', listar_provincias, name='listar_provincias'),
+    path('provincias/registrar/', agregar_provincia, name='agregar_provincia'), #type:ignore
+    path('provincias/editar/<pk>/', editar_provincia, name='editar_provincia'),
     path('provincias/buscar/', buscar_provincia, name='buscar_provincia'),
     #----------------------RUTAS PROVINCIAS----------------------#
 
     path('canton/listar/', listar_canton, name='listar_cantones'),
-    path('canton/registrar/', agregar_canton, name='agregar_canton'),
+    path('canton/registrar/', agregar_canton, name='agregar_canton'), #type:ignore
     path('canton/editar/<pk>', editar_canton, name='editar_canton'),
     path('canton/buscar/', buscar_canton, name='buscar_canton'),
     
-    path('tipo_fondo/registrar/', crear_tipo_fondo, name='crear_tipo_fondo'),
+    path('tipo_fondo/registrar/', crear_tipo_fondo, name='crear_tipo_fondo'), #type:ignore
     path('tipo_fondo/listar', listar_tipo_fondo, name='listar_tipo_fondo'),
     path('tipo_fondo/editar/<pk>/', editar_tipo_fondo, name='editar_tipo_fondo'),
     path('tipo_fondo/buscar', buscar_tipo_fondo, name='buscar_tipo_fondo'),
 
 
-
+    path('modulo/listar/', listar_modulos, name='listar_modulo'),
+    path('modulo/registrar/', agregar_modulo, name='agregar_modulo'),  #type:ignore
+    path('moduloe/ditar/<pk>', editar_canton, name='editar_modulo'),
+    path('modulo/buscar/', buscar_canton, name='buscar_modulo'),
 ]
---
+++ moduloFondo/vw_casanova.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import permission_required, login_required
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+
+from moduloFondo.forms.form_FND_modulo import *
+from moduloFondo.model.model_FND_modulo import *
+
+#--------------------------------------------------- FND_MODULOS ---------------------------------------------------#
+#LISTAR MODULOS
+@permission_required('moduloFondo.view_model_fnd_modulo')
+def listar_modulos(request):
+    pag_titulo = 'Lista de módulos'
+    modulo = Model_FND_modulo.objects.all()
+
+    paginator = Paginator(modulo, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, urls_modulo['listar'], {'title': pag_titulo, 'pagina_paginator': page_obj})
+#FIN LISTAR MODULOS
+
+
+#AGREGAR MÓDULO
+@permission_required('moduloFondo.view_model_fnd_modulo')
+def agregar_modulo(request):
+    pag_titulo = 'Registrar módulo'
+    frm_crear = Frm_Modulo
+
+    if request.method == 'GET':
+        return render(request, urls_modulo['crear'], {'title':pag_titulo,'frm':frm_crear})
+    if request.method  == 'POST':
+        frm_crear = Frm_Modulo(request.POST, request.FILES)
+        print( Frm_Modulo(request.POST, request.FILES))
+        if frm_crear.is_valid():
+            frm_crear.save()
+            messages.success(request, 'Se ha generado corectamente el formulario')
+            return redirect('fondo:listar_modulo')
+        else:
+            messages.warning(request, 'Se ha generado un error desconocido')
+            return render(request, urls_modulo['crear'], {'title':pag_titulo,'frm':frm_crear})
+#FIN AGREGAR MÓDULO
